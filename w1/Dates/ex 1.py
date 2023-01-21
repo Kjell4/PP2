245b2071b2bc866535648d951d1be45501ef847c
Kjell4 <ilias.bekturgan@mail.ru>
2023-01-21 22:52:38 +0600

    test commit

---
+++ w1/Dates/ex 1.py
@@ -0,0 +1,7 @@
+import datetime
+
+x = datetime.datetime(2018, 6, 1)
+
+print(x.strftime("%B"))         #month str
+print(x.strftime("%A"))         #Weekday full
+print(x.strftime("%Y"))         #year
